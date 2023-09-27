38742951df680298bba38ee3826096bc329a5345
hueftlein <sebastian.hueftlein@gmail.com>
2023-09-27 12:34:20 +0200

    Switch to Tailwind

--- public/app.js
+++ public/app.js
@@ -1,16 +1,38 @@
+const closeSvg = `
+<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M3 3L21 21" stroke="currentColor" stroke-width="3" stroke-linecap="round" />
+  <path d="M21 3L3 21" stroke="currentColor" stroke-width="3" stroke-linecap="round" />
+</svg>
+`;
+
+const addSvg = `
+<svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12 3L12 21" stroke="currentColor" stroke-width="3" stroke-linecap="round"/>
+  <path d="M21 12L3 12" stroke="currentColor" stroke-width="3" stroke-linecap="round"/>
+</svg>
+
+`;
+
 const codes = JSON.parse(localStorage.getItem("codes") || '["Hello"]');
 const history = JSON.parse(localStorage.getItem("history") || "[]");
 let activeTab = parseInt(localStorage.getItem("activeTab")) || 0;
 
+const getTabClass = (isActive) =>
+  `inline-flex flex-nowrap items-stretch border-r border-slate-900 dark:border-slate-300 ${isActive ? "bg-slate-100 dark:bg-slate-800 -mb-px" : ""
+  }`;
+
 const tabTemplate = (i) =>
   `
-    <li ${activeTab === i ? 'class="active"' : ""}>
-        <a href="#" onClick="goToTab(${i})" data-content="${i}"></a>
-        <a href="#" onClick="closeTab(${i})" class="close">✖</a>
+    <li class="${getTabClass(activeTab === i)}">
+        <a href="#" onClick="goToTab(${i})" data-content="${i}" class="p-4 pr-2 truncate"></a>
+        <a href="#" onClick="closeTab(${i})" class="p-4 pl-2 font-semibold text-xl inline-flex items-center">${closeSvg}</a>
     </li>
     `;
 
-const newTabTemplate = `<li><a href="#" class="new" onClick="newTab()">➕</a></li>`;
+const newTabTemplate = `
+<li class="${getTabClass()}">
+  <a href="#" class="p-4 font-semibold text-xl inline-flex items-center" onClick="newTab()" >${addSvg}</a>
+</li>`;
 
 const tabs = document.getElementById("tabs");
 
@@ -43,19 +65,20 @@ const updateTabContent = () => {
   document.getElementById("svg").replaceChildren(
     codes[activeTab]
       ? DATAMatrix({
-          msg: codes[activeTab],
-          dim: 256,
-          pad: 1,
-          pal: ["#000000", "#f2f4f8"],
-        })
+        msg: codes[activeTab],
+        dim: 256,
+        pad: 1,
+        pal: ["#000000", "#f2f4f8"],
+      })
       : ""
   );
 };
 
 const historyTemplate = (i) =>
   `
-    <li ${codes[activeTab] === history[i] ? 'class="active"' : ""}>
-        <a href="#" onClick="fromHistory(${i})" data-history-content="${i}"></a>
+  ${codes[activeTab] === history[i] ? "" : ""}
+    <li class="">
+        <a href="#" onClick="fromHistory(${i})" data-history-content="${i}" class="p-2 truncate block rounded hover:bg-slate-300"></a>
     </li>
     `;
 
@@ -69,20 +92,14 @@ const fillCodes = (queryAttr, fillAttr, source) => {
     element[fillAttr] =
       fillAttr === "href"
         ? "#" +
-          encodeURIComponent(
-            source[parseInt(element.getAttribute(queryAttr))] || ""
-          )
+        encodeURIComponent(
+          source[parseInt(element.getAttribute(queryAttr))] || ""
+        )
         : source[parseInt(element.getAttribute(queryAttr))] || "";
   });
 };
 
 const render = () => {
-  document
-    .querySelector(":root")
-    .style.setProperty(
-      "--tab-max-width",
-      `${Math.min(50, 100 / codes.length)}%`
-    );
   updateTabs();
   updateTabContent();
   updateHistory();
